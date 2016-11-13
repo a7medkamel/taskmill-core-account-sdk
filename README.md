c7f1aac128a948159e14b033cd9cac6298a2e8c9
Ahmed Kamel <ak@ahmedkamel.net>
2016-11-13 04:51:25 -0800

    initial code

---
+++ README.md
@@ -0,0 +1,2 @@
+# taskmill-core-account-sdk
+lib to make calls to taskmill-core-account service
---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var Promise         = require('bluebird')
+  , config          = require('config-url')
+  , _               = require('lodash')
+  , rp              = require('request-promise')
+  , urljoin         = require('url-join')
+  , errors          = require('request-promise/errors')
+  ;
+
+const url = config.has('account')? config.getUrl('account') : undefined;
+
+function issueTokenById(id, options) {
+  return Promise
+          .resolve(rp.get({ url : urljoin(url || options.url, '/account', id, '/token'), json : true }))
+          .then((result) => {
+            return result.data;
+          })
+          .catch(errors.StatusCodeError, { statusCode : 404 }, (err) => {
+            throw new Error('not found');
+          })
+          .catch(errors.StatusCodeError, (err) => {
+            throw new Error('not allowed');
+          });
+}
+
+function issueTokenByUsername(hostname, name, options) {
+  return Promise
+          .resolve(rp.get({ url : urljoin(url || options.url, '/account', hostname, name, '/token'), json : true }))
+          .then((result) => {
+            return result.data;
+          })
+          .catch(errors.StatusCodeError, { statusCode : 404 }, (err) => {
+            throw new Error('not found');
+          })
+          .catch(errors.StatusCodeError, (err) => {
+            throw new Error('not allowed');
+          });
+}
+
+module.exports = {
+    issueTokenById        : issueTokenById
+  , issueTokenByUsername  : issueTokenByUsername
+};
